3428f1d0b259939589474e43f8667dab647ce14d
Pertinaz <manuel.agudelo1@utp.edu.co>
2024-07-28 03:18:05 -0500

    creating home component

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Manuel Aguirre</h1>
+      <h3 className="home__subtitle">Backend Developer</h3>
+      <p className="home__description">
+        Commited and adaptable person, interested in the evolution of digital
+        technology and analysis, focused on the development of ideas and
+        solutions to today's commercial needs
+      </p>
+
+      <a href="#contact" className="button button--flex">
+        Say Hello
+      </a>
+    </div>
+  );
+}
+
+export default Data
