30415bfb7c2fd24a5bc42f2e8b0efab1165b17d9
Pertinaz <manuel.agudelo1@utp.edu.co>
2024-07-29 22:15:10 -0500

    building about component

---
+++ src/components/about/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './about.css'
+import Info from './Info';
+
+const About = () => {
+  return (
+    <section className="about section" id="about">
+      <h2 className="section__title">Who am I?</h2>
+      <span className="section__subtitle">Introduction</span>
+
+      <div className="about__container cntainer grid">
+        <div className="about__data">
+          <Info />
+
+          <p className="about__description"></p>
+          <a
+            href="https://cv-nine-flax.vercel.app/"
+            className="button button--flex"
+          >
+            Check my CV
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About
---
+++ src/components/about/Info.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about__info">
+      <div className="about__box">
+        <h3 className="about__title">Systems Engineering Student</h3>
+        <span className="about__subtitle">Web pasionate</span>
+      </div>
+
+      <div className="about__box">
+        <h3 className="about__title">Projects</h3>
+        <span className="about__subtitle">Building and deploying new exciting projects</span>
+      </div>
+
+      <div className="about__box">
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
+  );
+}
+
+export default Info
