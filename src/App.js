ba4360fe9024e37f52c42a8e0126451ca111ce7d
Pertinaz <manuel.agudelo1@utp.edu.co>
2024-07-30 23:26:34 -0500

    build the footer component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Skills from './components/skills/Skills';
-import Services from './components/services/Services';
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Services from "./components/services/Services";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      
-      <main className='main'>
+
+      <main className="main">
         <Home />
         <About />
         <Skills />
         <Services />
+        <Footer />
       </main>
     </>
   );
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Manuel</h1>
+
+        <div className="footer__reminder">
+          <span className="message">
+            Portfolio unfinished, adding new components and features while I
+            learned
+          </span>
+        </div>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="home" className="footer__link">
+              Try me
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
