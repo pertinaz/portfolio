7679608d17f9ae141b4a6b5f513c1f67ad167055
Pertinaz <manuel.agudelo1@utp.edu.co>
2024-08-04 00:15:08 -0500

    creating functional component for projects view

--- src/components/portfolio/ProjectCard.jsx
+++ src/components/portfolio/ProjectCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-
 const toolIcons = {
   react: "bx bxl-react",
   nodejs: "bx bxl-nodejs",
   css: "bx bxl-css3",
   postgresql: "bx bxl-postgresql",
+  checked: "bx bx-badge-check",
 };
 
 export const projects = [
   {
     title: "project 1",
     thumbnail: "url",
-    tools: ["react", "vercel", "css"],
+    tools: ["react", "nodejs", "css", "postgresql", "checked"],
   },
   {
     title: "project 2",
@@ -31,45 +31,31 @@ export const projects = [
   },
 ];
 
-export const ProjectCard = ({title,thumbnail,tools}) =>{
+export const ProjectCard = ({ title, thumbnail, tools }) => {
   return (
     <div className="projects__content">
       <h3 className="project__title">{title}</h3>
-      <img src={thumbnail} alt={`${title} thumbnail`} className="project__img" />
-      
+      <img
+        src={thumbnail}
+        alt={`${title} thumbnail`}
+        className="project__img"
+      />
+
       <div className="project__box">
         <div className="project__group">
-          <div className="project__data">
-            {tools.map((tool,index)=>(
+          {tools.map((tool, index) => (
+            <div className="project__data" key={index}>
               <i className={toolIcons[tool]}></i>
-              ))}
-          </div>
-
-          <div className="project__data">
-            <i class="bx bxl-postgresql"></i>
-            <div>
-              <h3 className="tool__name">PostgresSQL</h3>
+              <div>
+                <h3 className="tool__name">{tool}</h3>
+              </div>
             </div>
-          </div>
-
-          <div className="project__data">
-            <i class="bx bx-badge-check"></i>
-            <div>
-              <h3 className="tool__name">NestJS</h3>
-            </div>
-          </div>
-
-          <div className="project__data">
-            <i class="bx bx-badge-check"></i>
-            <div>
-              <h3 className="tool__name">Express</h3>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 /*
 
