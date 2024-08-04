8af98dd2576c7c083a7e58164e83b934e417d901
Pertinaz <manuel.agudelo1@utp.edu.co>
2024-08-04 02:04:38 -0500

    refactor footer and general component sizing

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -13,195 +13,197 @@ const Services = () => {
       <span className="section__subtitle">What I offer</span>
 
       <div className="services__container container grid">
-        <div className="services__content">
-          <div>
-            <i class="bx bxl-nodejs services__icon"></i>
-            <h3 className="services__title">
-              Server Side
-              <br />
-              Development
-            </h3>
-          </div>
-
-          <span className="services__button" onClick={() => toggleTab(1)}>
-            View More
-            <i className="uil uil-arrow-right services__button"></i>
-          </span>
-
-          <div
-            className={
-              toggleState === 1
-                ? "services__modal active-modal"
-                : "services__modal"
-            }
-          >
-            <div className="services__modal-content">
-              <i
-                onClick={() => toggleTab(0)}
-                className="uil uil-times services__modal-close"
-              ></i>
-              <h3 className="services__modal-title">
+        <div className="services__sort">
+          <div className="services__content">
+            <div>
+              <i class="bx bxl-nodejs services__icon"></i>
+              <h3 className="services__title">
                 Server Side
                 <br />
                 Development
               </h3>
-              <p className="services__modal-description">
-                I handle data processing, business logic, and ensuring the
-                proper execution of tasks.
-              </p>
+            </div>
 
-              <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Integration with Third-Party Services
-                  </p>
-                </li>
+            <span className="services__button" onClick={() => toggleTab(1)}>
+              View More
+              <i className="uil uil-arrow-right services__button"></i>
+            </span>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Server-Side Logic</p>
-                </li>
+            <div
+              className={
+                toggleState === 1
+                  ? "services__modal active-modal"
+                  : "services__modal"
+              }
+            >
+              <div className="services__modal-content">
+                <i
+                  onClick={() => toggleTab(0)}
+                  className="uil uil-times services__modal-close"
+                ></i>
+                <h3 className="services__modal-title">
+                  Server Side
+                  <br />
+                  Development
+                </h3>
+                <p className="services__modal-description">
+                  I handle data processing, business logic, and ensuring the
+                  proper execution of tasks.
+                </p>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Database Management</p>
-                </li>
+                <ul className="services__modal-services grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">
+                      Integration with Third-Party Services
+                    </p>
+                  </li>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Security, optimization & maintenance
-                  </p>
-                </li>
-              </ul>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">Server-Side Logic</p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">Database Management</p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">
+                      Security, optimization & maintenance
+                    </p>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="services__content">
-          <div>
-            <i class="bx bxl-react services__icon"></i>
-            <h3 className="services__title">
-              UI/UX <br />
-              designer
-            </h3>
-          </div>
-
-          <span className="services__button" onClick={() => toggleTab(2)}>
-            View More <i className="uil uil-arrow-right services__button"></i>
-          </span>
-
-          <div
-            className={
-              toggleState === 2
-                ? "services__modal active-modal"
-                : "services__modal"
-            }
-          >
-            <div className="services__modal-content">
-              <i
-                onClick={() => toggleTab(0)}
-                className="uil uil-times services__modal-close"
-              ></i>
-              <h3 className="services__modal-title">
+          <div className="services__content">
+            <div>
+              <i class="bx bxl-react services__icon"></i>
+              <h3 className="services__title">
                 UI/UX <br />
                 designer
               </h3>
-              <p className="services__modal-description">
-                create an intuitive, aesthetically pleasing, and user-friendly
-                interface that provides a positive experience for users.
-              </p>
+            </div>
 
-              <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Wireframing and Prototyping
-                  </p>
-                </li>
+            <span className="services__button" onClick={() => toggleTab(2)}>
+              View More <i className="uil uil-arrow-right services__button"></i>
+            </span>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    User Research and Testing
-                  </p>
-                </li>
+            <div
+              className={
+                toggleState === 2
+                  ? "services__modal active-modal"
+                  : "services__modal"
+              }
+            >
+              <div className="services__modal-content">
+                <i
+                  onClick={() => toggleTab(0)}
+                  className="uil uil-times services__modal-close"
+                ></i>
+                <h3 className="services__modal-title">
+                  UI/UX <br />
+                  designer
+                </h3>
+                <p className="services__modal-description">
+                  create an intuitive, aesthetically pleasing, and user-friendly
+                  interface that provides a positive experience for users.
+                </p>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Interaction Design</p>
-                </li>
+                <ul className="services__modal-services grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">
+                      Wireframing and Prototyping
+                    </p>
+                  </li>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Responsive Design</p>
-                </li>
-              </ul>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">
+                      User Research and Testing
+                    </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">Interaction Design</p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">Responsive Design</p>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="services__content">
-          <div>
-            <i class="bx bx-shield-alt-2 services__icon"></i>
-            <h3 className="services__title">
-              Ethical
-              <br />
-              Hacking
-            </h3>
-          </div>
-
-          <span className="services__button" onClick={() => toggleTab(3)}>
-            View More <i className="uil uil-arrow-right services__button"></i>
-          </span>
-
-          <div
-            className={
-              toggleState === 3
-                ? "services__modal active-modal"
-                : "services__modal"
-            }
-          >
-            <div className="services__modal-content">
-              <i
-                onClick={() => toggleTab(0)}
-                className="uil uil-times services__modal-close"
-              ></i>
-              <h3 className="services__modal-title">
+          <div className="services__content">
+            <div>
+              <i class="bx bx-shield-alt-2 services__icon"></i>
+              <h3 className="services__title">
                 Ethical
                 <br />
                 Hacking
               </h3>
-              <p className="services__modal-description">
-                Testing and evaluating the security of systems, networks, and
-                applications to identify and fix vulnerabilities before
-                malicious actors can exploit them.
-              </p>
+            </div>
 
-              <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Penetration Testing</p>
-                </li>
+            <span className="services__button" onClick={() => toggleTab(3)}>
+              View More <i className="uil uil-arrow-right services__button"></i>
+            </span>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Web Application Security
-                  </p>
-                </li>
+            <div
+              className={
+                toggleState === 3
+                  ? "services__modal active-modal"
+                  : "services__modal"
+              }
+            >
+              <div className="services__modal-content">
+                <i
+                  onClick={() => toggleTab(0)}
+                  className="uil uil-times services__modal-close"
+                ></i>
+                <h3 className="services__modal-title">
+                  Ethical
+                  <br />
+                  Hacking
+                </h3>
+                <p className="services__modal-description">
+                  Testing and evaluating the security of systems, networks, and
+                  applications to identify and fix vulnerabilities before
+                  malicious actors can exploit them.
+                </p>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Social Engineering</p>
-                </li>
+                <ul className="services__modal-services grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">Penetration Testing</p>
+                  </li>
 
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Network Security</p>
-                </li>
-              </ul>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">
+                      Web Application Security
+                    </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">Social Engineering</p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info">Network Security</p>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
